f493892a8c6ab35a0aa001fd56a6ba1cfc490cf3
dakata1337 <poshtazaprezentaciaa@gmail.com>
2024-02-15 16:15:32 +0200

    build: fix vscode debug setup

--- src/main.c
+++ src/main.c
@@ -1,3 +1,19 @@
+#include <stdio.h>
+
 int main(void) {
+    char file[] = "let a := 5\0";
+
+    char tokens[100];
+
+    for (int i = 0; i < (int)(sizeof(file) / sizeof(file[0])); i++) {
+        if (file[i] == '\0') {
+            break;
+        }
+
+        tokens[i] = file[i];
+    }
+
+    printf("%s\n", tokens);
+
     return 0;
 }
